ad0ab1f624c14e56673465e75e4da235843e187c
hrjugar <hernanjugar123@gmail.com>
2021-03-31 16:55:46 +0800

    Added Neil's part

--- neil_edit.py
+++ neil_edit.py
@@ -1,12 +1,11 @@
 import os
-from ctypes import windll
 from tkinter import *
-from tkinter import font
 from tkinter import ttk
+from tkinter import font
+from ctypes import windll
 
 # Improve text quality
 windll.shcore.SetProcessDpiAwareness(1)
-
 
 class MainFrame(Frame):
     def __init__(self, master):
@@ -20,10 +19,11 @@ class MainFrame(Frame):
 
         # Text box
         text_box = Text(self,
-                        font=("Consolas", 11),
+                        font=('Consolas', 11),
                         undo=True,
                         yscrollcommand=y_scrollbar.set,
                         selectbackground="#0078d7", selectforeground="white")
+
         text_box.pack(expand=True, fill=BOTH)
 
         # Adjust scrollbar to text box
@@ -88,7 +88,7 @@ class MainFrame(Frame):
         view_menu.add_command(label="Zoom In", accelerator="Ctrl+Plus", command=lambda: self.zoom_in(text_box))
         view_menu.add_command(label="Zoom Out", accelerator="Ctrl+Minus", command=lambda: self.zoom_out(text_box))
         view_menu.add_command(label="Restore Default Zoom", accelerator="Ctrl+0",
-                                command=lambda: self.restore_zoom(text_box))
+                              command=lambda: self.restore_zoom(text_box))
         status_bar_shown = BooleanVar()
         view_menu.add_checkbutton(label="Status Bar", onvalue=1, offvalue=0, variable=status_bar_shown,
                                   command=self.toggle_status_bar)
@@ -172,3 +172,4 @@ if __name__ == "__main__":
     root.geometry("700x600")
     main_frame = MainFrame(root)
     root.mainloop()
+
