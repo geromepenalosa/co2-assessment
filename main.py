38608b417c8f351b2aed486f169f5f7aad3ecf2a
hrjugar <hernanjugar123@gmail.com>
2021-03-31 17:05:20 +0800

    Made local variables in widgets method instance variables

--- main.py
+++ main.py
@@ -11,90 +11,98 @@ windll.shcore.SetProcessDpiAwareness(1)
 class MainFrame(Frame):
     def __init__(self, master):
         super().__init__(master)
+        # Initialize variables
+        self.y_scrollbar, self.text_box, self.menu_bar, \
+            self.file_menu, self.edit_menu, self.format_menu, self.view_menu, self.help_menu, \
+            self.var_wordwrap, self.var_status_bar_shown = [None] * 10
         self.widgets()
 
     def widgets(self):
         # Scrollbar
-        y_scrollbar = Scrollbar(self)
-        y_scrollbar.pack(side=RIGHT, fill=Y)
+        self.y_scrollbar = Scrollbar(self)
+        self.y_scrollbar.pack(side=RIGHT, fill=Y)
 
         # Text box
-        text_box = Text(self,
-                        font=("Consolas", 11),
-                        undo=True,
-                        yscrollcommand=y_scrollbar.set,
-                        selectbackground="#0078d7", selectforeground="white")
-        text_box.pack(expand=True, fill=BOTH)
+        self.text_box = Text(self,
+                             font=("Consolas", 11),
+                             undo=True,
+                             yscrollcommand=self.y_scrollbar.set,
+                             selectbackground="#0078d7", selectforeground="white")
+        self.text_box.pack(expand=True, fill=BOTH)
 
         # Adjust scrollbar to text box
-        y_scrollbar.config(command=text_box.yview)
+        self.y_scrollbar.config(command=self.text_box.yview)
 
         # Menu bar
-        menu_bar = Menu(self.master)
-        self.master.config(menu=menu_bar)
+        self.menu_bar = Menu(self.master)
+        self.master.config(menu=self.menu_bar)
 
         # Create menu cascades
-        file_menu = Menu(menu_bar, tearoff=False)
-        edit_menu = Menu(menu_bar, tearoff=False)
-        format_menu = Menu(menu_bar, tearoff=False)
-        view_menu = Menu(menu_bar, tearoff=False)
-        help_menu = Menu(menu_bar, tearoff=False)
+        self.file_menu = Menu(self.menu_bar, tearoff=False)
+        self.edit_menu = Menu(self.menu_bar, tearoff=False)
+        self.format_menu = Menu(self.menu_bar, tearoff=False)
+        self.view_menu = Menu(self.menu_bar, tearoff=False)
+        self.help_menu = Menu(self.menu_bar, tearoff=False)
 
         # Add menu cascades to menu bar
-        menu_bar.add_cascade(label="File", menu=file_menu)
-        menu_bar.add_cascade(label="Edit", menu=edit_menu)
-        menu_bar.add_cascade(label="Format", menu=format_menu)
-        menu_bar.add_cascade(label="View", menu=view_menu)
-        menu_bar.add_cascade(label="Help", menu=help_menu)
+        self.menu_bar.add_cascade(label="File", menu=self.file_menu)
+        self.menu_bar.add_cascade(label="Edit", menu=self.edit_menu)
+        self.menu_bar.add_cascade(label="Format", menu=self.format_menu)
+        self.menu_bar.add_cascade(label="View", menu=self.view_menu)
+        self.menu_bar.add_cascade(label="Help", menu=self.help_menu)
 
         # File menu commands
-        file_menu.add_command(label="New", accelerator="Ctrl+N", command=lambda: self.new_file(text_box))
-        file_menu.add_command(label="Open...", accelerator="Ctrl+O", command=lambda: self.open_file(text_box))
-        file_menu.add_command(label="Save", accelerator="Ctrl+S", command=lambda: self.save_file(text_box))
-        file_menu.add_command(label="Save As...", accelerator="Ctrl+Shift+S",
-                              command=lambda: self.save_as_file(text_box))
-        file_menu.add_separator()
-        file_menu.add_command(label="Exit", command=self.master.quit)
+        self.file_menu.add_command(label="New", accelerator="Ctrl+N", command=lambda: self.new_file(self.text_box))
+        self.file_menu.add_command(label="Open...", accelerator="Ctrl+O", command=lambda: self.open_file(self.text_box))
+        self.file_menu.add_command(label="Save", accelerator="Ctrl+S", command=lambda: self.save_file(self.text_box))
+        self.file_menu.add_command(label="Save As...", accelerator="Ctrl+Shift+S",
+                                   command=lambda: self.save_as_file(self.text_box))
+        self.file_menu.add_separator()
+        self.file_menu.add_command(label="Exit", command=self.master.quit)
 
         # Edit menu commands
-        edit_menu.add_command(label="Undo", accelerator="Ctrl+Z", command=text_box.edit_undo)
-        edit_menu.add_separator()
-        edit_menu.add_command(label="Cut", accelerator="Ctrl+X", state=DISABLED,
-                              command=lambda: self.master.focus_get().event_generate("<<Cut>>"))
-        edit_menu.add_command(label="Copy", accelerator="Ctrl+C", state=DISABLED,
-                              command=lambda: self.master.focus_get().event_generate("<<Copy>>"))
-        edit_menu.add_command(label="Paste", accelerator="Ctrl+V",
-                              command=lambda: self.master.focus_get().event_generate("<<Paste>>"))
-        edit_menu.add_command(label="Delete", accelerator="Del", state=DISABLED,
-                              command=lambda: self.master.focus_get().event_generate("<<Clear>>"))
-        edit_menu.add_separator()
-        edit_menu.add_command(label="Find...", accelerator="Ctrl+F", state=DISABLED,
-                              command=lambda: self.find_text(text_box))
-        edit_menu.add_command(label="Replace...", accelerator="Ctrl+H", command=lambda: self.replace_text(text_box))
-        edit_menu.add_separator()
-        edit_menu.add_command(label="Select All", accelerator="Ctrl+A",
-                              command=lambda: self.master.focus_get().event_generate("<<SelectAll>>"))
-        edit_menu.add_command(label="Time/Date", accelerator="F5", command=lambda: self.show_datetime(text_box))
+        self.edit_menu.add_command(label="Undo", accelerator="Ctrl+Z", command=self.text_box.edit_undo)
+        self.edit_menu.add_separator()
+        self.edit_menu.add_command(label="Cut", accelerator="Ctrl+X", state=DISABLED,
+                                   command=lambda: self.master.focus_get().event_generate("<<Cut>>"))
+        self.edit_menu.add_command(label="Copy", accelerator="Ctrl+C", state=DISABLED,
+                                   command=lambda: self.master.focus_get().event_generate("<<Copy>>"))
+        self.edit_menu.add_command(label="Paste", accelerator="Ctrl+V",
+                                   command=lambda: self.master.focus_get().event_generate("<<Paste>>"))
+        self.edit_menu.add_command(label="Delete", accelerator="Del", state=DISABLED,
+                                   command=lambda: self.master.focus_get().event_generate("<<Clear>>"))
+        self.edit_menu.add_separator()
+        self.edit_menu.add_command(label="Find...", accelerator="Ctrl+F", state=DISABLED,
+                                   command=lambda: self.find_text(self.text_box))
+        self.edit_menu.add_command(label="Replace...", accelerator="Ctrl+H",
+                                   command=lambda: self.replace_text(self.text_box))
+        self.edit_menu.add_separator()
+        self.edit_menu.add_command(label="Select All", accelerator="Ctrl+A",
+                                   command=lambda: self.master.focus_get().event_generate("<<SelectAll>>"))
+        self.edit_menu.add_command(label="Time/Date", accelerator="F5",
+                                   command=lambda: self.show_datetime(self.text_box))
 
         # Format menu commands
-        word_wrap = BooleanVar()
-        format_menu.add_checkbutton(label="Word Wrap", onvalue=1, offvalue=0, variable=word_wrap,
-                                    command=lambda: self.toggle_wrap(text_box, word_wrap))
-        format_menu.add_command(label="Font...", command=lambda: self.edit_font(text_box))
+        self.var_wordwrap = BooleanVar()
+        self.format_menu.add_checkbutton(label="Word Wrap", onvalue=1, offvalue=0, variable=self.var_wordwrap,
+                                         command=lambda: self.toggle_wrap(self.text_box, self.var_wordwrap))
+        self.format_menu.add_command(label="Font...", command=lambda: self.edit_font(self.text_box))
 
         self.pack(expand=True, fill=BOTH)
 
         # View menu commands
-        view_menu.add_command(label="Zoom In", accelerator="Ctrl+Plus", command=lambda: self.zoom_in(text_box))
-        view_menu.add_command(label="Zoom Out", accelerator="Ctrl+Minus", command=lambda: self.zoom_out(text_box))
-        view_menu.add_command(label="Restore Default Zoom", accelerator="Ctrl+0",
-                                command=lambda: self.restore_zoom(text_box))
-        status_bar_shown = BooleanVar()
-        view_menu.add_checkbutton(label="Status Bar", onvalue=1, offvalue=0, variable=status_bar_shown,
-                                  command=self.toggle_status_bar)
+        self.view_menu.add_command(label="Zoom In", accelerator="Ctrl+Plus",
+                                   command=lambda: self.zoom_in(self.text_box))
+        self.view_menu.add_command(label="Zoom Out", accelerator="Ctrl+Minus",
+                                   command=lambda: self.zoom_out(self.text_box))
+        self.view_menu.add_command(label="Restore Default Zoom", accelerator="Ctrl+0",
+                                   command=lambda: self.restore_zoom(self.text_box))
+        self.var_status_bar_shown = BooleanVar()
+        self.view_menu.add_checkbutton(label="Status Bar", onvalue=1, offvalue=0, variable=self.var_status_bar_shown,
+                                       command=self.toggle_status_bar)
 
         # Help commands
-        help_menu.add_command(label="About Clonepad")
+        self.help_menu.add_command(label="About Clonepad")
 
     def new_file(self, text_box):
         pass
@@ -144,7 +152,8 @@ class MainFrame(Frame):
         font_size.grid(row=1, column=3, padx=5, columnspan=2)
 
         # Button
-        confirm = Button(new_window, text="Ok", command=lambda: self.exit(text_box, font_group.get(), font_size.get(), new_window))
+        confirm = Button(new_window, text="Ok",
+                         command=lambda: self.exit(text_box, font_group.get(), font_size.get(), new_window))
         confirm.grid(row=4, column=9)
 
         cancel = Button(new_window, text="Cancel", command=lambda: new_window.destroy())
